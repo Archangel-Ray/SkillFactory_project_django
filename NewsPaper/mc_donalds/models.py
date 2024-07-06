07ba25c1a0a4d22e6b12b49662d69e75c2268950
Sergey Savelyev <sergey.savelyev2020@gmail.com>
2024-07-06 13:06:18 +0400

    поля модели "Staff" - Задание 5.5.3
    в таблице-модели "Staff" добавил три поля:
    full_name, position, labor_contract
    к полю "position" создал и прикрепил список значений

--- NewsPaper/mc_donalds/models.py
+++ NewsPaper/mc_donalds/models.py
@@ -11,8 +11,26 @@ class Product(models.Model):
 
 
 class Staff(models.Model):
+    # константы для списка должностей
+    director = 'DI'
+    admin = 'AD'
+    cook = 'CO'
+    cashier = 'CA'
+    cleaner = 'CL'
+
+    # список должностей
+    POSITIONS = [
+        (director, 'Директор'),
+        (admin, 'Администратор'),
+        (cook, 'Повар'),
+        (cashier, 'Кассир'),
+        (cleaner, 'Уборщик')
+    ]
+
     full_name = models.CharField(max_length=255)  # текст с ограничением
-    position = models.CharField(max_length=255)  # текст с ограничением
+    position = models.CharField(max_length=2,  # текст с ограничением
+                                choices=POSITIONS,  # прикреплённый список
+                                default=cashier)  # значение по умолчанию
     labor_contract = models.IntegerField()  # целое число
 
 
