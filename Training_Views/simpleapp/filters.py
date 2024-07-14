03b0607015232b878b00e830d9954a66dff11067
Sergey Savelyev <sergey.savelyev2020@gmail.com>
2024-07-14 17:06:57 +0300

    Модуль D7.2 - поле полного совпадения
    делал по деморолику, по этому получилось хаотично. (собственно, как и
    весь курс)
    
    добавил три варианта одного и того же поля, так как хотелось сохранить
    все образцы, а в ролике он всё замещал.
    есть обычное поле в классе "Meta" и два со списками настроек. один из
    них с возможностью фильтровать по одному полю, второй по нескольким.

--- Training_Views/simpleapp/filters.py
+++ Training_Views/simpleapp/filters.py
@@ -1,12 +1,36 @@
-from django_filters import FilterSet
+from django_filters import FilterSet, ModelChoiceFilter, ModelMultipleChoiceFilter
 
-from .models import Product
+from .models import Product, Category
 
 
 # Создаем свой набор фильтров для модели Product.
 # FilterSet, который мы наследуем,
 # должен чем-то напомнить знакомые вам Django дженерики.
 class ProductFilter(FilterSet):
+    # настройка поля фильтрации по конкретному значение
+    category_name_one_field = ModelChoiceFilter(
+        # имя поля в модели
+        field_name='category',
+        # список который будет отображаться в выпадающем списке
+        queryset=Category.objects.all(),
+        # название поля, которое будет отображаться
+        label='Категория товара',
+        # пустое поле в выпадающем списке
+        empty_label='любая'
+    )
+
+    # настройка поля фильтрации по нескольким значениям
+    category_name_more_field = ModelMultipleChoiceFilter(
+        # имя поля в модели
+        field_name='category',
+        # список который будет отображаться в списке для сортировки
+        queryset=Category.objects.all(),
+        # название поля, которое будет отображаться
+        label='Категория товара',
+        # True - "И"; по умолчанию = False - "ИЛИ"
+        conjoined=False
+    )
+
     class Meta:
         # В Meta классе мы должны указать Django модель,
         # в которой будем фильтровать записи.
@@ -22,4 +46,5 @@ class ProductFilter(FilterSet):
                 'lt',  # цена должна быть меньше или равна указанной
                 'gt',  # цена должна быть больше или равна указанной
             ],
+            'category': ['exact'],  # по списку значение полей
         }
