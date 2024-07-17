0f160f9030678b87f99b2053e60e4e9b59164cfc
Sergey Savelyev <sergey.savelyev2020@gmail.com>
2024-07-17 14:07:31 +0300

    Модуль D7.5 - пересоздание поля
    в лучших традициях СкиллФактори: так как мы делали мы делать не будем,
    а сделаем по-другому:
    вместо проверки заново объявляем поле с новыми параметрами

--- Training_Views/simpleapp/forms.py
+++ Training_Views/simpleapp/forms.py
@@ -5,6 +5,8 @@ from .models import Product
 
 
 class ProductForm(forms.ModelForm):
+    description = forms.CharField(min_length=20)
+
     class Meta:
         model = Product
         fields = [  # перечисление полей которые будут отображаться для редактирования
@@ -18,13 +20,9 @@ class ProductForm(forms.ModelForm):
     # проверка полей
     def clean(self):
         cleaned_data = super().clean()
-        description = cleaned_data.get("description")
-        if description is not None and len(description) < 20:
-            raise ValidationError({
-                "description": "Описание не может быть менее 20 символов."
-            })
-
         name = cleaned_data.get("name")
+        description = cleaned_data.get("description")
+
         if name == description:
             raise ValidationError(
                 "Описание не должно быть идентичным названию."
