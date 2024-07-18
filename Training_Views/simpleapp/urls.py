ee4891fce21bbff0523991d0c55a305b8e7c51d1
Sergey Savelyev <sergey.savelyev2020@gmail.com>
2024-07-18 16:21:07 +0300

    имена ссылкам
    в приложении добавил имена ссылкам

--- Training_Views/simpleapp/urls.py
+++ Training_Views/simpleapp/urls.py
@@ -10,10 +10,10 @@ urlpatterns = [
     # Т.к. наше объявленное представление является классом,
     # а Django ожидает функцию, нам надо представить этот класс в виде view.
     # Для этого вызываем метод as_view.
-    path('', ProductsList.as_view()),
+    path('', ProductsList.as_view(), name='product_list'),
     # pk — это первичный ключ товара, который будет выводиться у нас в шаблон
     # int — указывает на то, что принимаются только целочисленные значения
-    path('<int:pk>', ProductDetail.as_view()),
+    path('<int:pk>', ProductDetail.as_view(), name='product_detail'),
     # ссылка на представление редактирования продукта
     path('create/', create_product, name='product_create'),
 ]
