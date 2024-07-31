75ae7e455e4048fbccbda3636012c8130680ae59
Сергей Савельев <sergey-savelyev-2024@yandex.ru>
2024-07-31 09:12:33 +0300

    Модуль D8.5 - представление кнопки "Премиум"
    добавил функцию, которая проверяет, состоит ли пользователь
    в группе "Премиум" и добавляет в группу если не состоит.

--- simple_signup/sign/views.py
+++ simple_signup/sign/views.py
@@ -1,4 +1,6 @@
-from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User, Group
+from django.shortcuts import redirect
 from django.views.generic.edit import CreateView
 
 from .models import BaseRegisterForm
@@ -8,3 +10,15 @@ class BaseRegisterView(CreateView):
     model = User
     form_class = BaseRegisterForm
     success_url = '/'
+
+
+@login_required
+def upgrade_me(request):
+    """
+    Добавление в группу "Премиум"
+    """
+    user = request.user
+    premium_group = Group.objects.get(name='premium')
+    if not request.user.groups.filter(name='premium').exists():
+        premium_group.user_set.add(user)
+    return redirect('/')
