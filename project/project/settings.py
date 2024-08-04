b83ae06dc29e026ac47d08e7fc511bf06be3ef19
Сергей Савельев <sergey-savelyev-2024@yandex.ru>
2024-08-04 12:56:51 +0300

    Модуль D9.3 - промежуточные
    интересно что означает это "промежуточные"...

--- project/project/settings.py
+++ project/project/settings.py
@@ -57,6 +57,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    # Добавьте промежуточное программное обеспечение учетной записи:
+    "allauth.account.middleware.AccountMiddleware",
 ]
 
 ROOT_URLCONF = 'project.urls'
