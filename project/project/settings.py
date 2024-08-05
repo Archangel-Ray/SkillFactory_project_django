bf16d6b8b56184d2ae015697cb3063ae4f343047
Сергей Савельев <sergey-savelyev-2024@yandex.ru>
2024-08-05 12:29:26 +0300

    Модуль D9.4 - изменил настройки загрузки приложения
    сделал загрузку приложения в настройках так как показано в уроке.
    проверил. сигнал срабатывает и на создание и на изменение записи.

--- project/project/settings.py
+++ project/project/settings.py
@@ -36,7 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'appointment',
+    'appointment.apps.AppointmentConfig',
     'django.contrib.sites',
 
     'allauth',
