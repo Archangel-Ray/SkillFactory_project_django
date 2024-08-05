28fe291792e4155106c1069f0036b731186f5eb4
Сергей Савельев <sergey-savelyev-2024@yandex.ru>
2024-08-05 19:04:49 +0300

    Модуль D9.4 - подключение периодических задач
    запуск сервера без тестирования один раз
    если внесены изменения автоматического перезапуска не будет
    сервер надо перезапускать, чтоб изменения вошли в силу.
    
    python manage.py runserver --noreload

--- project/appointment/apps.py
+++ project/appointment/apps.py
@@ -7,3 +7,20 @@ class AppointmentConfig(AppConfig):
 
     def ready(self):
         import appointment.signals
+
+        from .tasks import send_mails
+        from .scheduler import appointment_schedulers
+        print('started')  # для отображения момента запуска периодической задачи
+
+        appointment_schedulers.add_job(
+            id='mail send',
+            func=send_mails,
+            trigger='interval',
+            seconds=10,
+        )
+        appointment_schedulers.start()
+
+        # запуск сервера без тестирования один раз:
+        # python manage.py runserver 8010 --noreload
+        # если внесены изменения автоматического перезапуска не будет
+        # сервер надо перезапускать, чтоб изменения вошли в силу.
