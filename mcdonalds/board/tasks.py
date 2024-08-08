1dbb851fd84034d75d32e50251b29c93d7ac6fc7
Сергей Савельев <sergey-savelyev-2024@yandex.ru>
2024-08-08 11:52:07 +0300

    Модуль D10.4 - скопировал задачу
    написали, что надо перезапустить Селери.
    а как? (вопрос Архитектору)

--- mcdonalds/board/tasks.py
+++ mcdonalds/board/tasks.py
@@ -1,1 +1,8 @@
 from celery import shared_task
+import time
+
+
+@shared_task
+def hello():
+    time.sleep(10)
+    print("Тут СкиллФактори посылает Хелло на Ворлд! (с восклицательным знаком, обязательно)")
