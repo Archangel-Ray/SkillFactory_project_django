c519d791e6a7e9e0ee40140316caf150ce331c17
Сергей Савельев <sergey-savelyev-2024@yandex.ru>
2024-08-08 13:42:21 +0300

    Модуль D10.4 - момент истины
    вернул представление в изначальное положение. и теперь задача есть
    
    всё отлично отработалось. так запутано объясняли, такую простую
    вещь. СкиллФактори Супер! Архитектор великолепен.
    __________________________________________________________________
    увидел подтверждение своей догадки про часовые пояса.
    между оформлением заказа и выполнением ровно три часа. хотя
    должно быть одно и тоже, потому что я нажимал кнопку забрать
    через пару секунд.
    разбираться сейчас не хочу, но буду иметь ввиду.

--- mcdonalds/board/tasks.py
+++ mcdonalds/board/tasks.py
@@ -1,15 +1,14 @@
 from celery import shared_task
 import time
 
-
-@shared_task
-def hello():
-    time.sleep(10)
-    print("Тут СкиллФактори посылает Хелло на Ворлд! (с восклицательным знаком, обязательно)")
+from .models import Order
 
 
 @shared_task
-def printer(N):
-    for i in range(N):
-        time.sleep(1)
-        print(i + 1)
+def complete_order(oid):
+    order = Order.objects.get(pk=oid)
+    order.complete = True
+    order.save()
+
+# запуск задач
+# celery -A mcdonalds worker -l INFO --pool=solo
--- mcdonalds/board/views.py
+++ mcdonalds/board/views.py
@@ -1,24 +1,17 @@
-from django.http import HttpResponse
 from django.shortcuts import redirect
-from django.views import View
 from django.views.generic import TemplateView, CreateView
-from .tasks import hello, printer  # , complete_order
+from .tasks import complete_order
 from .models import Order
-from datetime import datetime, timedelta
+from datetime import datetime
 
 
-class IndexView(View):  # TemplateView):
-    def get(self, request):
-        printer.apply_async([10],
-                            eta=datetime.now() + timedelta(seconds=5))
-        hello.delay()
-        return HttpResponse('Ещё один Халло! (но этого уже не посылали)')
-    # template_name = "board/index.html"
-    #
-    # def get_context_data(self, **kwargs):
-    #     context = super().get_context_data(**kwargs)
-    #     context['orders'] = Order.objects.all()
-    #     return context
+class IndexView(TemplateView):
+    template_name = "board/index.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['orders'] = Order.objects.all()
+        return context
 
 
 class NewOrderView(CreateView):
@@ -30,7 +23,7 @@ class NewOrderView(CreateView):
         order = form.save()
         order.cost = sum([prod.price for prod in order.products.all()])
         order.save()
-        # complete_order.apply_async([order.pk], countdown=5)
+        complete_order.apply_async([order.pk], countdown=5)
         return redirect('/')
 
 
