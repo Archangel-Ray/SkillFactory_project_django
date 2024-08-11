eb7f54b97c7578badd892353c7260100d0073807
Сергей Савельев <sergey-savelyev-2024@yandex.ru>
2024-08-11 13:50:53 +0300

    Модуль D11.3 - кеширование класса представления
    пример кеширования представления класса.
    __________________________________________________________________
    вот теперь заметил изменение. страница отображения товара после
    редактирование не изменилась.
    наверно, таки файлы кеша надо было в игнор лист закинуть.

--- Training_Views/simpleapp/urls.py
+++ Training_Views/simpleapp/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.views.decorators.cache import cache_page
 
 # Импортируем созданное нами представление
 from .views import ProductsList, ProductDetail, ProductCreate, ProductUpdate, ProductDelete
@@ -13,7 +14,8 @@ urlpatterns = [
     path('', ProductsList.as_view(), name='product_list'),
     # pk — это первичный ключ товара, который будет выводиться у нас в шаблон
     # int — указывает на то, что принимаются только целочисленные значения
-    path('<int:pk>', ProductDetail.as_view(), name='product_detail'),
+    # добавим кэширование на детали товара. Раз в 10 минут товар будет записываться в кэш для экономии ресурсов.
+    path('<int:pk>/', cache_page(60*10)(ProductDetail.as_view()), name='product_detail'),
     # ссылка на представление редактирования продукта
     path('create/', ProductCreate.as_view(), name='product_create'),
     path('<int:pk>/update/', ProductUpdate.as_view(), name='product_update'),
