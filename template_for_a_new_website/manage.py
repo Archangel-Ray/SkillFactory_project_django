41eb85fe398e70807fe85d050405896be516fc89
Сергей Савельев <sergey-savelyev-2024@yandex.ru>
2024-08-15 16:33:00 +0300

    Модуль D14.2 - тег должен быть обязательно {{project_name}}
    проект может иметь собственное название, а вот директория
    настроек и все места где должно быть вписано название проекта
    должно быть тегом {{project_name}} а секретный код {{secret_key}}
    __________________________________________________________________
    в скринкасте сказано, что название надо взять в двойные скобки.
    это не верно. название заменяется тегом project_name.

--- template_for_a_new_website/manage.py
+++ template_for_a_new_website/manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', '{{template_for_a_new_website}}.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', '{{project_name}}.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
