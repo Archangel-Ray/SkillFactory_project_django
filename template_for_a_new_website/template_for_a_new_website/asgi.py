9ad644ee0c0f1fed2844628d40438eb101053fe3
Сергей Савельев <sergey-savelyev-2024@yandex.ru>
2024-08-15 12:33:27 +0300

    Модуль D14.2 - шаблон для нового сайта - пустой проект
    создал новый Джанго проект из которого сейчас сделаю шаблон.

---
+++ template_for_a_new_website/template_for_a_new_website/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for template_for_a_new_website project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'template_for_a_new_website.settings')
+
+application = get_asgi_application()
