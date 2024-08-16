a76733774711e7f1d9b044a7527e6e67d1b97b3f
Сергей Савельев <sergey-savelyev-2024@yandex.ru>
2024-08-16 12:25:01 +0300

    Модуль D14.2 - приложение по шаблону - превращаем в шаблон
    все имена этого приложения в этом приложении
    меняем на тег {{app_name}}

--- create_project_using_template/application_template/apps.py
+++ create_project_using_template/application_template/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class ApplicationTemplateConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'application_template'
+    name = '{{app_name}}'
