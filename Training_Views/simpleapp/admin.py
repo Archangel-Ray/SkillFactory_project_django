c8eea4d9f0bcf9838ac389f40e1885318eb32723
Сергей Савельев <sergey-savelyev-2024@yandex.ru>
2024-08-18 09:37:00 +0300

    Модуль D14.5 - добавил свойство в модель и в админ-панель

--- Training_Views/simpleapp/admin.py
+++ Training_Views/simpleapp/admin.py
@@ -6,7 +6,7 @@ from .models import Category, Product
 # создаём новый класс для представления товаров в админке
 class ProductAdmin(admin.ModelAdmin):
     # list_display — это список или кортеж со всеми полями, которые вы хотите видеть в таблице с товарами
-    list_display = ('name', 'price')  # оставляем только имя и цену товара
+    list_display = ('name', 'price', 'on_stock')  # оставляем только имя и цену товара
 
 
 admin.site.register(Category)
