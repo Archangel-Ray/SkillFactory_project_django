dbecc18aeb94a91be82802b60f67ce6305925f3e
Сергей Савельев <sergey-savelyev-2024@yandex.ru>
2024-08-18 09:54:28 +0300

    Модуль D14.5 - операции с выделенным в админ-панели
    добавил функцию обнуления поля количества и
    добавил её строку "Действия"

--- Training_Views/simpleapp/admin.py
+++ Training_Views/simpleapp/admin.py
@@ -3,12 +3,25 @@ from django.contrib import admin
 from .models import Category, Product
 
 
+# напишем уже знакомую нам функцию обнуления товара на складе
+def nullfy_quantity(modeladmin, request, queryset):
+    # все аргументы уже должны быть вам знакомы, самые нужные из них это
+    # request — объект хранящий информацию о запросе и
+    # queryset — грубо говоря набор объектов, которых мы выделили галочками.
+    queryset.update(quantity=0)
+
+
+# описание для более понятного представления в админ панели задаётся, как будто это объект
+nullfy_quantity.short_description = 'Обнулить товары'
+
+
 # создаём новый класс для представления товаров в админке
 class ProductAdmin(admin.ModelAdmin):
     # list_display — это список или кортеж со всеми полями, которые вы хотите видеть в таблице с товарами
-    list_display = ('name', 'price', 'on_stock')  # оставляем только имя и цену товара
+    list_display = ('name', 'price', 'on_stock', 'quantity')  # оставляем только имя и цену товара
     list_filter = ('price', 'quantity', 'name')  # добавляем примитивные фильтры в нашу админку
     search_fields = ('name', 'category__name')  # тут всё очень похоже на фильтры из запросов в базу
+    actions = [nullfy_quantity]  # добавляем действия в список
 
 
 admin.site.register(Category)
