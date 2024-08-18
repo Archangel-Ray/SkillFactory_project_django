d28a538e1aa57d98edda30bbbe4e09d7d4eb8773
Сергей Савельев <sergey-savelyev-2024@yandex.ru>
2024-08-18 09:30:45 +0300

    Модуль D14.5 - регистрация в админ-панели с конкретными полями

--- Training_Views/simpleapp/admin.py
+++ Training_Views/simpleapp/admin.py
@@ -6,8 +6,7 @@ from .models import Category, Product
 # создаём новый класс для представления товаров в админке
 class ProductAdmin(admin.ModelAdmin):
     # list_display — это список или кортеж со всеми полями, которые вы хотите видеть в таблице с товарами
-    list_display = [field.name for field in Product._meta.get_fields()]
-    # генерируем список имён всех полей для более красивого отображения
+    list_display = ('name', 'price')  # оставляем только имя и цену товара
 
 
 admin.site.register(Category)
