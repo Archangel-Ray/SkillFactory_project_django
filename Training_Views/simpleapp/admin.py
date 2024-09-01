f83387dd316c5ba499521b04e27a3aafde6438e5
Сергей Савельев <sergey-savelyev-2024@yandex.ru>
2024-09-01 11:50:27 +0300

    Модуль D17.4 - регистрация переводчика в админ-панели
    вот не уверен, что я правильно сделал. потом придётся исправлять.

--- Training_Views/simpleapp/admin.py
+++ Training_Views/simpleapp/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-
+# импортируем модель админ-панели (вспоминаем модуль про переопределение стандартных админ-инструментов)
+from modeltranslation.admin import TranslationAdmin
 from .models import Category, Product
 
 
@@ -16,12 +17,17 @@ nullfy_quantity.short_description = 'Обнулить товары'
 
 
 # создаём новый класс для представления товаров в админке
-class ProductAdmin(admin.ModelAdmin):
+class ProductAdmin(admin.ModelAdmin, TranslationAdmin):
     # list_display — это список или кортеж со всеми полями, которые вы хотите видеть в таблице с товарами
     list_display = ('name', 'price', 'on_stock', 'quantity')  # оставляем только имя и цену товара
     list_filter = ('price', 'quantity', 'name')  # добавляем примитивные фильтры в нашу админку
     search_fields = ('name', 'category__name')  # тут всё очень похоже на фильтры из запросов в базу
     actions = [nullfy_quantity]  # добавляем действия в список
+
+
+# Регистрируем модели для перевода в админ-панели
+class CategoryAdmin(TranslationAdmin):
+    model = Category
 
 
 admin.site.register(Category)
