9ada12912f471154555f007e9fa56cb304931e57
Сергей Савельев <sergey-savelyev-2024@yandex.ru>
2024-09-08 11:26:04 +0300

    пример на основе базового класса REST Framework - GET запрос

--- Dj_REST_frame_site/women/views.py
+++ Dj_REST_frame_site/women/views.py
@@ -4,11 +4,19 @@ https://www.django-rest-framework.org/api-guide/generic-views/
 """
 from django.shortcuts import render
 from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from .models import Women
 from .serializers import WomenSerializer
 
 
-class WomenAPIView(generics.ListAPIView):
-    queryset = Women.objects.all()  # считывание всех данных из модели
-    serializer_class = WomenSerializer  # назначение сериализатора
+class WomenAPIView(APIView):  # наследуемся от базового класса
+    # обработчик GET запросов
+    def get(self, request):
+        return Response({'title': 'Анджелина Джоли'})
+
+
+# class WomenAPIView(generics.ListAPIView):
+#     queryset = Women.objects.all()  # считывание всех данных из модели
+#     serializer_class = WomenSerializer  # назначение сериализатора
