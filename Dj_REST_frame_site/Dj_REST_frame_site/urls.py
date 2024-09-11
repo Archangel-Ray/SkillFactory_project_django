438f1c95db49198a3334b14f39e8dcd6b0fb144a
Сергей Савельев <sergey-savelyev-2024@yandex.ru>
2024-09-11 11:49:22 +0300

    создание роутера

--- Dj_REST_frame_site/Dj_REST_frame_site/urls.py
+++ Dj_REST_frame_site/Dj_REST_frame_site/urls.py
@@ -16,8 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from rest_framework import routers  # генератор путей
 
 from women.views import *
+
+# создаётся объект, который будет формировать текущие пути относительно представления
+router = routers.SimpleRouter()
 
 urlpatterns = [
     path('admin/', admin.site.urls),
