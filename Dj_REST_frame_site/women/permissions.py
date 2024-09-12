9afe599e6641a79be7d0829a11dead4ed7b4c638
Сергей Савельев <sergey-savelyev-2024@yandex.ru>
2024-09-12 11:08:05 +0300

    скопировал из документации класс проверки пользователя
    возвращает "Истина" дла безопасных запросов.
    в остальных случаях сопоставляет пользователя из объекта и запроса
    вернётся "Истина" если запрашивает пользователь создавший объект.

--- Dj_REST_frame_site/women/permissions.py
+++ Dj_REST_frame_site/women/permissions.py
@@ -7,3 +7,19 @@ class IsAdminOrReadOnly(permissions.BasePermission):
             return True
 
         return bool(request.user and request.user.is_staff)
+
+
+class IsOwnerOrReadOnly(permissions.BasePermission):
+    """
+    Разрешение на уровне объекта, позволяющее редактировать его только владельцам объекта.
+    Предполагается, что экземпляр модели имеет атрибут «владелец».
+    """
+
+    def has_object_permission(self, request, view, obj):
+        # Разрешения на чтение разрешены для любого запроса,
+        # поэтому мы всегда разрешаем запросы GET, HEAD или OPTIONS.
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        # Экземпляр должен иметь атрибут с именем «владельца».
+        return obj.user == request.user
